1456eca2d3651ba124176732bf60864aed2a7c4e
dimi-fn <dimitris_fanis@hotmail.com>
2020-12-24 13:38:40 +0200

    init

--- Unit_testing/smallest_positive.py
+++ Unit_testing/smallest_positive.py
@@ -6,26 +6,27 @@ def solution(x):
     if max(x) <= 0 or (max(x)==2 and min(x)!=1):
         return 1
 
-    # If nothing of the above occurs then: sort the list and keep only the positive numbers
-    x.sort()
-    sorted_positive_list = [num for num in x if num>0]
-
-    if min(sorted_positive_list) != 1:
-        return 1
     else:
-        '''
-        - Find the 1st occurrence of the pair of integers where the difference is greater than 2,
-        and then return the smallest positive number that does not exist in the list,
-        e.g. if x= [1,2,3,5] then return 4
-        - Else, return the max plus one,
-        e.g. if x=[1,2,3], then return 4
-        '''
-        for i in range(len(sorted_positive_list)-1):
-            difference = sorted_positive_list[i+1] - sorted_positive_list[i]
-            if difference >=2:
-                return sorted_positive_list[i] + 1
-        return max(sorted_positive_list)+1
+      # If nothing of the above occurs then: sort the list and keep only the positive numbers
+      x.sort()
+      sorted_positive_list = [num for num in x if num>0]
 
+      if min(sorted_positive_list) != 1:
+          return 1
+      else:
+          '''
+          - Find the 1st occurrence of the pair of integers where the difference is greater than 2,
+          and then return the smallest positive number that does not exist in the list,
+          e.g. if x= [1,2,3,5] then return 4
+          - Else, return the max plus one,
+          e.g. if x=[1,2,3], then return 4
+          '''
+          for i in range(len(sorted_positive_list)-1):
+              difference = sorted_positive_list[i+1] - sorted_positive_list[i]
+              if difference >=2:
+                  return sorted_positive_list[i] + 1
+          return max(sorted_positive_list)+1
+          
 # Testing:
 class Test_solution(unittest.TestCase):
 
