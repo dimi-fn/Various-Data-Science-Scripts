622068c32b13fc633347bfb1655243d23be21294
dimi-fn <dimitris_fanis@hotmail.com>
2021-01-11 02:16:02 +0200

    fix code

--- Unit_testing/Codility/Caterpillar_method/test_solution.py
+++ Unit_testing/Codility/Caterpillar_method/test_solution.py
@@ -6,17 +6,12 @@ import solution_AbsDistinct
 class TestSample(unittest.TestCase):
 
     def test_codility_example(self):
-
         self.assertEqual(solution_AbsDistinct.solution([-5,-3,-1,0,3,6]), 5)
 
-
     def test_Only_Negatives(self):
-
         self.assertEqual(solution_AbsDistinct.solution([-5,-3,-1,-2,-3,-6]), 5)
-
     
     def test_mix(self):
-
         self.assertEqual(solution_AbsDistinct.solution([-100, 99, 100, 1, 3, -3, 8, -55, 56, -99]), 7)
 
 if __name__ == "__main__":
