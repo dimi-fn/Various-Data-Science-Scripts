142910889ecdd0e3da61653a7764dc2c74502c40
dimi-fn <dimitris_fanis@hotmail.com>
2021-02-20 23:41:27 +0200

    OOP .py files for importing class as a module

---
+++ OOP/car.py
@@ -0,0 +1,29 @@
+class Car:
+  '''
+  * Creating a class called "Car"
+  * Properties/attributes: brand, colour, horses, country production, current speed
+  * Method definition: 
+    * def move_car() moves the car by 10
+    * def accelerate_car() accelerates the car by the given value and adds speed to "current_speed"
+    * def stop_car() sets "current_speed" to 0
+    * def car_details returns the properties of the "Car" class
+  '''
+
+  def __init__(self, brand, colour, horses, current_speed, country_production = "Germany"):
+    self.brand = brand
+    self.colour= colour
+    self.horses = horses
+    self.current_speed = current_speed
+    self.country_production = country_production
+
+  def move_car(self):
+    self.current_speed += 10
+
+  def accelerate_car(self, value):
+    self.current_speed += value
+
+  def stop_car(self):
+    self.current_speed = 0
+
+  def car_details(self):
+    print ("Brand: {}\nColour: {}\nHorses: {}\nCountry production: {}\nCurrent speed: {}\n".format(self.brand, self.colour, self.horses, self.country_production, self.current_speed))
---
+++ OOP/get_car_details.py
@@ -0,0 +1,19 @@
+# from car.py import the class "Car"
+from car import Car
+
+print("The doc string of the above class is:\n")
+print(Car.__doc__)
+
+new_car = Car(brand="Mercedes", colour="black", horses= 200, current_speed= 60)
+print(new_car)
+new_car.car_details()
+new_car.move_car()
+new_car.current_speed
+new_car.accelerate_car(80)
+new_car.current_speed
+new_car.stop_car()
+new_car.current_speed
+new_car.move_car()
+new_car.current_speed
+new_car.accelerate_car(280)
+new_car.car_details()
