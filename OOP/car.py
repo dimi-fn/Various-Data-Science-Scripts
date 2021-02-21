aa5e9d73df90871268f56a742efe918678426264
dimi-fn <dimitris_fanis@hotmail.com>
2021-02-21 04:44:40 +0200

    updated car.py

--- OOP/car.py
+++ OOP/car.py
@@ -1,21 +1,24 @@
 class Car:
   '''
   * Creating a class called "Car"
+
   * Properties/attributes: brand, colour, horses, country production, current speed 
-    * "current_speed" is set 0, unless other value is assigned
-  * Method definition: 
+    * "current_speed" is set to 0, unless other value is assigned
+
+  * Method definitions: 
     * def move_car() moves the car by 10
-    * def accelerate_car() accelerates the car by the given value and adds speed to "current_speed"
+    * def accelerate_car() accelerates the car by the given value and adds that speed to "current_speed"
     * def stop_car() sets "current_speed" to 0
-    * def car_details returns the properties of the "Car" class
+    * def car_details() returns the properties of the "Car" class
   '''
 
   def __init__(self, brand, colour, horses, country_production, current_speed = 0):
     self.brand = brand
     self.colour= colour
     self.horses = horses
-    self.current_speed = current_speed
     self.country_production = country_production
+    self.current_speed = current_speed
+    
 
   def move_car(self):
     self.current_speed += 10
@@ -27,4 +30,5 @@ class Car:
     self.current_speed = 0
 
   def car_details(self):
-    print ("Brand: {}\nColour: {}\nHorses: {}\nCountry production: {}\nCurrent speed: {}\n".format(self.brand, self.colour, self.horses, self.country_production, self.current_speed))
+    print ("Brand: {}\nColour: {}\nHorses: {}\nCountry production: {}\nCurrent speed: {}\n".format(
+        self.brand, self.colour, self.horses, self.country_production, self.current_speed))
