655931a9a7dcdf0a4e13d873dc5340408491f021
dimi-fn <dimitris_fanis@hotmail.com>
2021-02-25 10:32:51 +0200

    updated sqlite3.py

--- Databases/sqlite3_dbapi.py
+++ Databases/sqlite3_dbapi.py
@@ -2,13 +2,18 @@ import sqlite3
 
 def main():
     
-    # connect to sqlite3 db api
-    print('connect')
+    # connecting to sqlite3 
+    ''' 
+    connection returns a db handle that can be used to interface with db
+    and this can be managed via the db.cursor() 
+    '''
+    print('Connecting...')
+    print("**************************************************************\n")
     db = sqlite3.connect('db-api.db')
     cur = db.cursor()
-    print('create')
-
+    
     ''' Create table '''
+    print('Creating tables:\n')
     # first check if table exists
     cur.execute("DROP TABLE IF EXISTS test")
     cur.execute("""
@@ -18,32 +23,38 @@ def main():
         """)
     
     # Insert rows
-    print('Inserting a row')
+    print('Inserting 3 rows')
     cur.execute("""
         INSERT INTO test (string, number) VALUES ('Mercedes', 25000)
         """)
-    print('insert row')
     cur.execute("""
         INSERT INTO test (string, number) VALUES ('BMW', 20000)
         """)
-    print('insert row')
     cur.execute("""
         INSERT INTO test (string, number) VALUES ('Audi', 18000)
         """)
     
     # Commit
-    print('commit')
+    print('Inserts were committed')
     db.commit()
-    print('count')
+
+    # Query - count rows
     cur.execute("SELECT COUNT(*) FROM test")
     count = cur.fetchone()[0]
-    print(f'there are {count} rows in the table.')
-    print('read')
+    print(f'There are {count} rows in the table')
+
+    # Query - print rows
+    print("\nPrinting the table's rows:")
     for row in cur.execute("SELECT * FROM test"):
         print(row)
-    print('drop')
+
+    # drop table  
+    print("**************************************************************\n")  
+    print("Dropping the 'test' table")
     cur.execute("DROP TABLE test")
-    print('close')
+
+    # Closing db
     db.close()
+    print('db closed')
 
 if __name__ == '__main__': main()
