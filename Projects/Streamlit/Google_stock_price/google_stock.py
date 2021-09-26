2c2bd3e48ccdeb92ad1094e9e8e4920821d098a1
dimi-fn <dimitris_fanis@hotmail.com>
2021-09-27 01:56:36 +0300

    updated google_stocks.py

--- Projects/Streamlit/Google_stock_price/google_stock.py
+++ Projects/Streamlit/Google_stock_price/google_stock.py
@@ -1,9 +1,9 @@
-import yfinance as yf
 import streamlit as st
+import yfinance as yf
 
 st.write("""
 # Simple Stock Price App
-Shown are the stock closing price and volume of Google!
+Shown are the stock **closing price** and ***volume*** of Google!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
@@ -15,5 +15,11 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
