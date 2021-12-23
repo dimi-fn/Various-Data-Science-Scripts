bb13cd986190752b03adc4c19d54e250bb4bb818
dimi-fn <dimitris_fanis@hotmail.com>
2021-12-23 19:47:23 +0200

    updated web development

--- Web Development/JavaScript/learn_JavaScript.js
+++ Web Development/JavaScript/learn_JavaScript.js
@@ -17,3 +17,17 @@ console.log('middle' + ' ' + 'space');
 
 // Properties
 console.log("hello".length);
+
+// Methods
+console.log('hello'.toUpperCase()); // Prints 'HELLO'
+console.log('Hey'.startsWith('H')); // Prints true
+console.log('    Remove whitespace   '.trim()); // Removes the whitespace at the beginning and end of the string
+
+// Built-in Objects
+console.log(Math.floor(10/3)) // takes a decimal number, and rounds down to the nearest whole number
+console.log(Math.floor(Math.random() * 50)+ "\n"); // Prints a random whole number between 0 and 50
+console.log(Number.isInteger(2017))
+
+// Variables
+var x=5;
+console.log(x);
