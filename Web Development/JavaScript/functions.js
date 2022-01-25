f1b7b7ecd58807a40b4df1cbb3d111504233eff2
dimi-fn <dimitris_fanis@protonmail.com>
2022-01-25 19:22:39 +0200

    updated JavaScript

--- Web Development/JavaScript/functions.js
+++ Web Development/JavaScript/functions.js
@@ -44,28 +44,30 @@ console.log("\n");
 
 /******************* Function Expressions *******************/
 const isWeekend = function(day){
-    if (day==="Saturday" || day==="Sunday"){
-      return "Yea! It is " + day + ", so it is weekend!";
+  if (day==="Saturday" || day==="Sunday"){
+    return "Yea! It is " + day + ", so it is weekend!";
     } else {
       return "It is a weekday because it is " + day;
     }
   };
-  isWeekend("Tuesday")
-  console.log(isWeekend('Tuesday'));
-  isWeekend("Sunday")
-  console.log(isWeekend('Sunday'));
-  console.log("\n");
+isWeekend("Tuesday")
+console.log(isWeekend('Tuesday'));
+isWeekend("Sunday")
+console.log(isWeekend('Sunday'));
+console.log("\n");
 
-  /******************* Arrow Functions *******************/
-  let rectangle_Area = (width, height) => {
-    let area = width * height;
-    return area;
-  };
-  console.log(rectangle_Area(5,3));
+/******************* Arrow Functions *******************/
+// without arrow function:
+function rectangle_area(width, height){
+  let area=width*height;
+  return area;
+}
+rect_area = rectangle_area(5,3);
+console.log(rect_area);
 
-  function rectangleArea(width, height){
-    let area=width*height;
-    return area;
-  }
-  rectangle_Area = rectangleArea(5,3);
-  console.log(rectangle_Area);
+// arrow function:
+let rectangle_area_arrow = (width, height) => {
+  let rect_area = width * height;
+  return rect_area;
+};
+console.log(rectangle_area_arrow(5,3));
