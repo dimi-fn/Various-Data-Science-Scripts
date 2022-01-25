9aa78b8f6d8c50cb435d277ceaa090e1d6b1fb17
dimi-fn <dimitris_fanis@protonmail.com>
2022-01-25 10:31:39 +0200

    updated JavaScript

--- Web Development/JavaScript/arrays.js
+++ Web Development/JavaScript/arrays.js
@@ -16,3 +16,27 @@ console.log(`The 2nd and 3rd car in the array are: ${car_array.slice(1,3)}`); //
 
 //indices
 console.log(`The index of mercedes car in the array is: ${car_array.indexOf("mercedes")}`); // Python: print(car_array.index("mercedes"))
+
+/**************** Arrays and Functions ****************
+When you pass an array into a function, if the array is mutated inside the function,
+that change will be maintained outside the function as well (concept: pass-by-reference)
+*/
+
+var someArray = ["a", "b", "c", "d"];
+function delLastElement(arr){
+    /*Parementer: array
+    Returns: the array with its last item deleted
+    */
+    arr.pop();
+}
+delLastElement(someArray); // argument passed: someArray
+console.log(someArray);
+
+/* In Python:
+someArray = ["a", "b", "c", "d"]
+def delLastElement(arr):
+  arr.pop()
+delLastElement(someArray)
+print(someArray)  
+*/
+
