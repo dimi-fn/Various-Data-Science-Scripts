856d21f44e5ba8cb597f5d0430d2d81f2ddca1ab
dimi-fn <dimitris_fanis@protonmail.com>
2023-07-22 16:05:59 +0100

    fixed json validity script

--- Various Scripts/JSON_validity/check_if_json_file_is_valid.py
+++ Various Scripts/JSON_validity/check_if_json_file_is_valid.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-
 # Check JSON validity
 def is_valid_json_file(file_name):
     try:
@@ -15,11 +14,11 @@ def is_valid_json_file(file_name):
         print(f"Error while reading the JSON file '{file_name}': {value_err}")
     return False
 
-# Check a JSON file
-# This will work if the JSON file is in the same directory path with this Python script
+# Check a particular JSON file
+# This will work if the JSON file is in the same directory path with this Python script, else adjust the path
 # replace the value of file_name with your actual JSON file name
 file_name = "json_test_file_not_uploaded.json"
 if is_valid_json_file(file_name):
      print(f"The function returned {is_valid_json_file(file_name)} so the provided JSON file is valid!")
 else:
-     print(f"The function returned {is_valid_json_file(file_name)} so the provided JSON file is valid!")
+     print(f"The function returned {is_valid_json_file(file_name)} so the provided JSON file is not valid!")
