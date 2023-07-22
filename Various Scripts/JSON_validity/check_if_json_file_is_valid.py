adc3daafefec759a2ee426646d71074924b67435
dimi-fn <dimitris_fanis@protonmail.com>
2023-07-22 16:03:57 +0100

    uploaded various scripts/check json validity

---
+++ Various Scripts/JSON_validity/check_if_json_file_is_valid.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+
+# Check JSON validity
+def is_valid_json_file(file_name):
+    try:
+        file_path = os.path.join(os.path.dirname(__file__), file_name)
+        with open(file_path, 'r') as file:
+            json.load(file)
+            return True
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+    except ValueError as value_err:
+        print(f"Error while reading the JSON file '{file_name}': {value_err}")
+    return False
+
+# Check a JSON file
+# This will work if the JSON file is in the same directory path with this Python script
+# replace the value of file_name with your actual JSON file name
+file_name = "json_test_file_not_uploaded.json"
+if is_valid_json_file(file_name):
+     print(f"The function returned {is_valid_json_file(file_name)} so the provided JSON file is valid!")
+else:
+     print(f"The function returned {is_valid_json_file(file_name)} so the provided JSON file is valid!")
---
+++ Various Scripts/README.md
@@ -0,0 +1,17 @@
+# Various Scripts
+
+
+Contents
+=======================
+
+* [Check if a JSON file is valid](#check-if-a-json-file-is-valid)
+
+
+------- 
+
+
+# Check if a JSON file is valid
+
+* Find the python script [here](), replace the file name with your own JSON file name and run 
+
+```python check_if_json_file_is_valid.py``
