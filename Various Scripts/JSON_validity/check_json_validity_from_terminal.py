2c114434792d3e93c410a65f948e5f3c5b0b21c5
dimi-fn <dimitris_fanis@protonmail.com>
2023-07-24 11:58:46 +0100

    updated various scripts/json validity

---
+++ Various Scripts/JSON_validity/check_json_validity_from_terminal.py
@@ -0,0 +1,29 @@
+import json
+import os
+import sys
+
+# Check JSON validity
+def is_valid_json_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            json.load(file)
+            return True
+    except FileNotFoundError:
+        print(f"Error: File '{file_name}' not found.")
+    except ValueError as value_err:
+        print(f"Error while reading the JSON file '{file_name}': {value_err}")
+    return False
+
+if __name__ == "__main__":
+    # if uses hasn't given 2 arguments
+    if len(sys.argv) != 2:
+        print("You didn't enter the right command. Run: python check_json_validity.py <your-json-filename>.json")
+        sys.exit(1)
+
+    # the file_name is the second comand line argument
+    file_name = sys.argv[1]
+
+    if is_valid_json_file(file_name):
+        print(f"The provided JSON file '{file_name}' is valid!")
+    else:
+        print(f"The provided JSON file '{file_name}' is not valid!")
--- Various Scripts/README.md
+++ Various Scripts/README.md
@@ -12,8 +12,29 @@ Contents
 
 # Check if a JSON file is valid
 
+## 1st way
+
 * Find the python script [here](https://github.com/dimi-fn/Various-Data-Science-Scripts/blob/main/Various%20Scripts/JSON_validity/check_if_json_file_is_valid.py), replace the file name with your own JSON file name and run: 
 
 ```
 python check_if_json_file_is_valid.py
 ```
+
+## 2nd way
+
+* Find the python script [here]()
+* Have you JSON script in the same directory with the Python script above
+* Run:
+
+```
+python check_json_validity_from_terminal.py <your-JSON-file-name>.json
+```
+
+## 3rd way
+
+* Navigate to the directory when your JSON file is and run
+
+```
+python -c "import json; json.load(open('<your-json-file-name-here>.json')); print('The JSON file is valid')"
+```
+If the file is valid then it will print 'The JSON file is valid', else the corresponding errors.
